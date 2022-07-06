ab1286445d7d281efec8b338b9cf6526906f8154
sunb <sunb@inflab.com>
2022-07-06 10:50:29 +0900

    여러 큐 한번에 생성 가능하도록 수정, DLQ 설정 추가

--- main.tf
+++ main.tf
@@ -1,31 +1,42 @@
+resource "aws_sqs_queue" "dlq" {
+  for_each                   = {for q in var.queues : "${try(q.name, "")}${try(q.name_prefix, "")}" => q if try(q.dlq, true)}
+  name                       = try("${each.value.name}-dlq", null)
+  name_prefix                = try("${each.value.name_prefix}-dlq", null)
+  visibility_timeout_seconds = try(each.value.visibility_timeout_seconds, null)
+  message_retention_seconds  = try(each.value.message_retention_seconds, null)
+}
+
 resource "aws_sqs_queue" "this" {
-  count = var.create ? 1 : 0
+  for_each = {for q in var.queues : "${try(q.name, "")}${try(q.name_prefix, "")}" => q}
 
-  name        = var.name
-  name_prefix = var.name_prefix
-
-  visibility_timeout_seconds  = var.visibility_timeout_seconds
-  message_retention_seconds   = var.message_retention_seconds
-  max_message_size            = var.max_message_size
-  delay_seconds               = var.delay_seconds
-  receive_wait_time_seconds   = var.receive_wait_time_seconds
-  policy                      = var.policy
-  redrive_policy              = var.redrive_policy
-  redrive_allow_policy        = var.redrive_allow_policy
-  fifo_queue                  = var.fifo_queue
-  content_based_deduplication = var.content_based_deduplication
-  deduplication_scope         = var.deduplication_scope
-  fifo_throughput_limit       = var.fifo_throughput_limit
-
-  kms_master_key_id                 = var.kms_master_key_id
-  kms_data_key_reuse_period_seconds = var.kms_data_key_reuse_period_seconds
+  name                       = try(each.value.name, null)
+  name_prefix                = try(each.value.name_prefix, null)
+  visibility_timeout_seconds = try(each.value.visibility_timeout_seconds, null)
+  message_retention_seconds  = try(each.value.message_retention_seconds, null)
+  max_message_size           = try(each.value.max_message_size, null)
+  delay_seconds              = try(each.value.delay_seconds, null)
+  receive_wait_time_seconds  = try(each.value.receive_wait_time_seconds, null)
+  policy                     = try(each.value.policy, null)
+  redrive_policy             = try(each.value.dlq, true) ? jsonencode({
+    deadLetterTargetArn = aws_sqs_queue.dlq[each.key].arn,
+    maxReceiveCount     = try(each.value.max_receive_count, 4)
+  }) : null
+  redrive_allow_policy = try(each.value.dlq, true) ? jsonencode({
+    redrivePermission = "byQueue",
+    sourceQueueArns   = [aws_sqs_queue.dlq[each.key].arn]
+  }) : null
+  fifo_queue                        = try(each.value.fifo_queue, null)
+  content_based_deduplication       = try(each.value.content_based_deduplication, null)
+  deduplication_scope               = try(each.value.deduplication_scope, null)
+  fifo_throughput_limit             = try(each.value.fifo_throughput_limit, null)
+  kms_master_key_id                 = try(each.value.kms_master_key_id, null)
+  kms_data_key_reuse_period_seconds = try(each.value.kms_data_key_reuse_period_seconds, null)
 
   tags = var.tags
 }
 
 data "aws_arn" "this" {
-  count = var.create ? 1 : 0
+  for_each = {for q in var.queues : "${try(q.name, "")}${try(q.name_prefix, "")}" => q}
 
-  arn = aws_sqs_queue.this[0].arn
-
+  arn = aws_sqs_queue.this[each.key].arn
 }
--- outputs.tf
+++ outputs.tf
@@ -1,32 +1,19 @@
 output "sqs_queue_id" {
   description = "The URL for the created Amazon SQS queue"
-  value = element(
-    concat(
-      aws_sqs_queue.this.*.id,
-      [""],
-    ),
-    0,
-  )
+  value       = {for k, v in aws_sqs_queue.this : k => v.id}
 }
 
 output "sqs_queue_arn" {
   description = "The ARN of the SQS queue"
-  value = element(
-    concat(
-      aws_sqs_queue.this.*.arn,
-      [""],
-    ),
-    0,
-  )
+  value       = {for k, v in aws_sqs_queue.this : k => v.arn}
+}
+
+output "sqs_dlq_arn" {
+  description = "The ARN of the Dead Letter SQS queue"
+  value       = {for k, v in aws_sqs_queue.dlq : k => v.arn}
 }
 
 output "sqs_queue_name" {
   description = "The name of the SQS queue"
-  value = element(
-    concat(
-      data.aws_arn.this.*.resource,
-      [""],
-    ),
-    0,
-  )
+  value       = {for k, v in data.aws_arn.this : k => v.resource}
 }
